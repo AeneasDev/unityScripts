80352126d446c88f2f4c3c2256778a3743c8d26f
AeneasDev <73922794+AeneasDev@users.noreply.github.com>
2020-11-06 12:15:56 -0800

    Update and rename DS.cs to obstical.cs
    changed name

--- obstical.cs
+++ obstical.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DS : MonoBehaviour
+public class obstical : MonoBehaviour
 {
     public Rigidbody rb;
     public int s;
