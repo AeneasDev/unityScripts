303af67658c02a46f242678d8cc76413e908d6ce
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-11-06 16:29:09 +0800

    Update PlayerMovementMaster.cs

--- movement pack/PlayerMovementMaster.cs
+++ movement pack/PlayerMovementMaster.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 namespace MovementPack {
 	public class PlayerMovementMaster : MonoBehaviour {
-		public Transform t;
-		public Rigidbody rb;
+		public Transform trans;
+		public Rigidbody rigidBody;
 		public int movementSpeed;
 		public int rotationSpeed;
-		public int h;
+		public int jumpHeight;
 		public bool isGrounded;
 		
 		private void OnCollisionEnter(Collision collision){
@@ -25,31 +25,31 @@ namespace MovementPack {
 
 		private void Update() {
 			if (Input.GetKey(KeyCode.A)){
-				t.Rotate(0, -rSpeed * Time.deltaTime, 0);
+				trans.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
 			}
 			
 			if (Input.GetKey(KeyCode.D)){
-				t.Rotate(0, rSpeed * Time.deltaTime, 0);
+				trans.Rotate(0, rotationSpeed * Time.deltaTime, 0);
 			}
 			
 			if (Input.GetKey(KeyCode.W)){
-				rb.AddRelativeForce(Vector3.forward * mSpeed);
+				rigidBody.AddRelativeForce(Vector3.forward * movementSpeed);
 			}
 			
 			if (Input.GetKey(KeyCode.S)){
-				rb.AddRelativeForce(-Vector3.forward * mSpeed);
+				rigidBody.AddRelativeForce(-Vector3.forward * movementSpeed);
 			}
 			
 			if (isGrounded){
 				if (Input.GetKey(KeyCode.Space)) {
-					rb.AddForce(0, h, 0);
+					rigidBody.AddForce(0, h, 0);
 					
 					isGrounded = true;
 				}
 			}
 				
 			if (!isGrounded){
-					t.Rotate(0, 100, 0);
+			        trans.Rotate(0, 100, 0);
 			}
 		}
 	}
