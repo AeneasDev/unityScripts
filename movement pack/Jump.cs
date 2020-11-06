879997dd09cd85f0ed55bfbaa8f1e3a4747d60c0
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-11-06 15:42:55 +0800

    Update Jump.cs

--- movement pack/Jump.cs
+++ movement pack/Jump.cs
@@ -2,26 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jump : MonoBehaviour
-{
-	public string objectName;
-	public Rigidbody rb;
-	public int h;
-	int onornot = 0;
-    // Start is called before the first frame update
-    void OnCollisionEnter(Collision col)
-    {
-    	if(col.gameObject.name == objectName){
-    		Debug.Log("On ground statement is true.");
-    		onornot = 1;
-    	}
-    }
-    void Update(){
-    	if (onornot == 1){
-    		if (Input.GetKey("space")){
-    			rb.AddForce(0, h, 0);
-    			onornot = 0;
+namespace MovementPack {
+	public class Jump : MonoBehaviour {
+		public string objectName;
+		public Rigidbody rigidBody;
+		public int h;
+		private bool _onGround = true;
+		
+    	private void OnCollisionEnter(Collision col)
+    	{
+    		if(col.gameObject.name == objectName) {
+    			Debug.Log("On ground statement is true.");
+    			onornot = false;
     		}
     	}
-    }
+		
+    	private void Update(){
+    		if (onGround) {
+    			if (Input.GetKey("space")) {
+    				rigidBody.AddForce(0, h, 0);
+    				onGround = true;
+    			}
+    		}
+    	}
+	}
 }
