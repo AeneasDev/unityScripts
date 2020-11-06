7fa42338e6c442be88ebd9d7ed128e16089e0ec5
AeneasDev <73922794+AeneasDev@users.noreply.github.com>
2020-11-05 23:48:43 -0800

    Update PMM.cs
    merged jump and pmm together

--- movement pack/PMM.cs
+++ movement pack/PMM.cs
@@ -10,12 +10,25 @@ public class PMM : MonoBehaviour
 
 	public Transform t;
 	public Rigidbody rb;
-	public float mSpeed;
+	public int mSpeed;
 	public int rSpeed;
+	public int h;
+	int onornot = 0;
     // Start is called before the first frame update
-    void Start()
+    public bool isGrounded;
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.layer == 0 && !isGrounded){
+            isGrounded = true;
+            onornot = 1;
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == 0 && isGrounded){
+            isGrounded = false;
+            onornot = 0;
+        }
     }
 
     // Update is called once per frame
@@ -33,5 +46,16 @@ public class PMM : MonoBehaviour
         if (Input.GetKey("s")){
         	rb.AddRelativeForce(-Vector3.forward * mSpeed);
         }
+        if (isGrounded == true){
+    		if (Input.GetKey("space")){
+    			rb.AddForce(0, h, 0);
+    			onornot = 0;
+    		}
+    	if (isGrounded == false){
+    			t.Rotate(0, 100, 0);
+    			
+    	}
+        }
     }
 }
+
