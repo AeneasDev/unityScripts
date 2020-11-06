e04a801a4d3cafef3aba99fc78574dd8a87295ff
AeneasDev <73922794+AeneasDev@users.noreply.github.com>
2020-11-06 12:14:54 -0800

    Add files via upload

---
+++ DS.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DS : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int s;
+    int correction = 1;
+    void Start()
+    {
+	        
+    }
+
+    // Update is called once per frame
+    IEnumerator Move()
+    {
+        rb.AddRelativeForce(Vector3.left * s);
+
+       	yield return new WaitForSeconds(1);
+       	rb.AddRelativeForce(Vector3.left * s);
+        rb.AddRelativeForce(Vector3.right * s * 2);
+        yield return new WaitForSeconds(1);
+	}
+	void Update()
+	{
+		StartCoroutine(Move());
+	}
+}
