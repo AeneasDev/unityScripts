2689db98a8ab2b34d1e6fe21e5095be0dae9d8ca
Drifty6 <alvadovil@gmail.com>
2020-11-13 21:28:54 +0100

    Uploaded the entire project
    Just what I wrote.

---
+++ Plataformer/Platformer (1)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+	
+	#endregion
+	#region Unity Methods
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	#endregion
+}
---
+++ Plataformer/Platformer (1)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables and Properties
+    private bool _isGrounded;
+    private bool _isAlive;
+    private float Xrotation;
+    private float Yrotation;
+    [Header("Ground check parameters")]
+    [SerializeField] private Transform groundcheckBase;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+	#endregion
+	#region Unity Methods
+    void Start()
+    {
+        _isAlive = true;
+        Xrotation = transform.rotation.y;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ground check
+        _isGrounded = Physics.CheckSphere(groundcheckBase.position, groundCheckRadius, groundLayer);
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    #endregion
+}
